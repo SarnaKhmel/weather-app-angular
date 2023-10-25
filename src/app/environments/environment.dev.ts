b7337735f869c60ca212cdd1ef69cd1e29bb2fe4
sarnakhmel <OlSa@olsa.local>
2023-10-25 19:46:46 +0300

    fin

---
+++ src/app/environments/environment.dev.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: false,
+    apiUrl: 'https://weather-api99.p.rapidapi.com/weather',
+    XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+    XRapidAPIHostHeaderValue: 'weather-api99.p.rapidapi.com',
+    XRapidAPIKeyHeaderName: "X-RapidAPI-Key",
+    XRapidAPIKeyHeaderValue: "187007c725msh960d8ca1454f77bp11497bjsn808a4d7bf281",
+  };
--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient} from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { environment } from '../environments/environment.dev';
+import { Observable } from 'rxjs';
+import { WeatherData } from '../models/weather.model';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +11,14 @@ export class WeatherService {
 
   constructor(private http: HttpClient) { }
 
-  getWeatherData(cityName: string){
-    this.http.get(`https://open-weather-map27.p.rapidapi.com/weather`)
+  getWeatherData(cityName: string): Observable<WeatherData> { 
+    const params = new HttpParams().set('city', cityName);
+
+    return this.http.get<WeatherData>(environment.apiUrl, { 
+      headers: new HttpHeaders()
+        .set(environment.XRapidAPIHostHeaderName, environment.XRapidAPIHostHeaderValue)
+        .set(environment.XRapidAPIKeyHeaderName, environment.XRapidAPIKeyHeaderValue),
+      params: params
+    });
   }
 }
